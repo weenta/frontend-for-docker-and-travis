09e8bbfc87f6197d54e7287c6e185897495833c0
wentao.li <wentao.li@thoughtworks.com>
2021-08-27 07:02:02 +0800

    split frontend out from main proj

---
+++ src/App.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hi/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/hi/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
