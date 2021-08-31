79dfab5471b3c957a8ded3529abdf8dad92768e1
wentao.li <wentao.li@thoughtworks.com>
2021-08-31 08:34:16 +0800

    Update app

--- README.md
+++ README.md
@@ -3,6 +3,3 @@
 A react application for docker learning
 
 
-
-access_key_id: $AWS_ACCESS_KEY
-secret_access_key: $AWS_SECRET_KEY
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Hi there
+        Hi there2
       </header>
     </div>
   );
